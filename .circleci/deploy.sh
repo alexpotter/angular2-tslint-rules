2fdc0944a87291a9d7c5c5d02381b224aa771db9
Alex Potter <alex.potter1993@gmail.com>
2019-05-31 16:03:07 +0100

    ???

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -7,6 +7,6 @@ NPM_TOKEN=$2
 
 case "$BRANCH" in
     master)
-        /home/circleci/angular2-tslint-rules/deploy-npm.sh
+        /home/circleci/angular2-tslint-rules/.circleci/deploy-npm.sh
         ;;
 esac
