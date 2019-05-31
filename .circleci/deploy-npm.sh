b483c1c1e9c9c4f2c7aabf5784dd9005e281013c
Alex Potter <alex.potter1993@gmail.com>
2019-05-31 16:09:00 +0100

    And

--- .circleci/deploy-npm.sh
+++ .circleci/deploy-npm.sh
@@ -4,5 +4,5 @@ set -eu
 
 NPM_TOKEN=$1
 
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > /home/circleci/angular2-tslint-rules/
 npm publish
